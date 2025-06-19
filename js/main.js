8c036b506739a78a3fec3e15499ab0eef93022e5
Sergio Silva <sergiosilvamarti@gmail.com>
2025-06-19 09:40:28 -0500

    Sergio Silva | 2025-06-19 | Agregada animación de rebote disparada por evento click con JS

--- js/main.js
+++ js/main.js
@@ -18,3 +18,15 @@ const observer = new IntersectionObserver(
 );
 
 cajas.forEach((caja) => observer.observe(caja));
+
+// Animación con click (JS)
+const botonRebote = document.getElementById("boton-rebote");
+
+botonRebote.addEventListener("click", () => {
+  botonRebote.classList.add("rebotar");
+
+  // Remover la clase para poder volver a hacer la animación en próximos clics
+  setTimeout(() => {
+    botonRebote.classList.remove("rebotar");
+  }, 600); // misma duración que la animación
+});
